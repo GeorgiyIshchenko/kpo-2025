df40134bb28bab8200cec7baaa8694e621fd1c91
Georgiy Ishchenko <gogaigd2005@gmail.com>
2025-01-29 23:36:25 +0300

    changes to satisfy checkstyle

--- practice/practise-3/src/main/java/hse/kpo/KpoApplication.java
+++ practice/practise-3/src/main/java/hse/kpo/KpoApplication.java
@@ -1,10 +1,12 @@
 package hse.kpo;
 
 import javax.swing.Spring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hello world app.
+ */
 @SpringBootApplication
 public class KpoApplication {
     public static void main(String[] args) {
--- practice/practise-3/src/main/java/hse/kpo/services/CustomerStorage.java
+++ practice/practise-3/src/main/java/hse/kpo/services/CustomerStorage.java
@@ -1,21 +1,21 @@
 package hse.kpo.services;
 
 import hse.kpo.domains.Customer;
-import hse.kpo.interfaces.ICustomerProvider;
-import org.springframework.stereotype.Component;
-
-import org.springframework.stereotype.Component;
-
+import hse.kpo.interfaces.InCustomerProvider;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
+/**
+ * Класс для хранения покупателей.
+ */
 @Component
-public class CustomerStorage implements ICustomerProvider {
-    private final List<Customer> customers = new ArrayList<>();
+public class CustomerStorage implements InCustomerProvider {
+    private List<Customer> customers = new ArrayList<>();
 
     /**
      * Метод для получения всех покупателей из списка customers.
-     * 
+     *
      * @return - список покупателей
      */
     @Override
@@ -25,11 +25,10 @@ public class CustomerStorage implements ICustomerProvider {
 
     /**
      * Метод для добавления покупателя.
-     * 
-     * @param customer - покупатель.
+     *
+     * @param customer - покупатель
      */
-    public void addCustomer(Customer customer)
-    {
+    public void addCustomer(Customer customer) {
         customers.add(customer); // просто добавляем покупателя в список
     }
 }
