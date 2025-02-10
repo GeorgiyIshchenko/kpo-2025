9a5a44fb21d89cae51b266c8b3f296cdcf7f2f39
Georgiy Ishchenko <gogaigd2005@gmail.com>
2025-02-10 20:55:11 +0300

    practice-5 classwork commit

--- practice/practise-5/src/main/java/hse/kpo/services/CarService.java
+++ practice/practise-5/src/main/java/hse/kpo/services/CarService.java
@@ -2,8 +2,10 @@ package hse.kpo.services;
 
 import hse.kpo.domains.Car;
 import hse.kpo.domains.Customer;
+import hse.kpo.domains.ProductionTypes;
 import hse.kpo.interfaces.ICarFactory;
 import hse.kpo.interfaces.ICarProvider;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.List;
 @Component
 public class CarService implements ICarProvider {
 
+    @Getter
     private final List<Car> cars = new ArrayList<>();
 
     private int carNumberCounter = 0;
--- practice/practise-5/src/main/java/hse/kpo/services/HseShipService.java
+++ practice/practise-5/src/main/java/hse/kpo/services/HseShipService.java
@@ -1,4 +1,32 @@
 package hse.kpo.services;
 
-public interface HseShipService {
+import hse.kpo.interfaces.ICarProvider;
+import hse.kpo.interfaces.ICustomerProvider;
+import hse.kpo.interfaces.IShipProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class HseShipService {
+
+    private final IShipProvider carProvider;
+
+    private final ICustomerProvider customerProvider;
+
+    public void sellShips()
+    {
+        // получаем список покупателей
+        var customers = customerProvider.getCustomers();
+        // пробегаемся по полученному списку
+        customers.stream().filter(customer -> Objects.isNull(customer.getCar()))
+                .forEach(customer -> {
+                    var ship = carProvider.takeShip(customer);
+                    if (Objects.nonNull(ship)) {
+                        customer.setShip(ship);
+                    }
+                });
+    }
 }
