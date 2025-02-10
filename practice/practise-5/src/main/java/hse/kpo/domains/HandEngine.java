a0468385cef8d5bc20b2228d1cd59b4b4cef892d
Georgiy Ishchenko <gogaigd2005@gmail.com>
2025-02-10 20:55:11 +0300

    practice-5 classwork commit

--- practice/practise-5/src/main/java/hse/kpo/domains/HandEngine.java
+++ practice/practise-5/src/main/java/hse/kpo/domains/HandEngine.java
@@ -2,11 +2,15 @@ package hse.kpo.domains;
 
 import hse.kpo.interfaces.IEngine;
 import lombok.ToString;
+import org.springframework.boot.logging.java.JavaLoggingSystem;
 
 @ToString
 public class HandEngine implements IEngine {
     @Override
-    public boolean isCompatible(Customer customer) {
-        return customer.getHandPower() > 5;
+    public boolean isCompatible(Customer customer, ProductionTypes type) {
+        return switch (type) {
+            case ProductionTypes.CAR -> customer.getHandPower() > 5;
+            case ProductionTypes.CATAMARAN -> customer.getHandPower() > 2;
+        };
     }
 }
---
+++ practice/practise-5/src/main/java/hse/kpo/report/ReportBuilder.java
@@ -0,0 +1,57 @@
+package hse.kpo.report;
+
+import hse.kpo.domains.Car;
+import hse.kpo.domains.Customer;
+import hse.kpo.domains.Ship;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class ReportBuilder {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public ReportBuilder() {
+        content = new StringBuilder();
+    }
+
+    public ReportBuilder addCustomers(List<Customer> customers)
+    {
+        content.append("Покупатели:");
+        customers.forEach(customer -> content.append(String.format(" - %s", customer)));
+        content.append("\n");
+
+        return this;
+    }
+
+    public ReportBuilder addCars(List<Car> cars) {
+        content.append("Добавлены автомобили:");
+        cars.forEach(car -> content.append(String.format(" - %s", car)));
+        content.append("\n");
+        return this;
+    }
+
+    public ReportBuilder addShips(List<Ship> ships) {
+        content.append("Добавлены катеры:");
+        ships.forEach(ship -> content.append(String.format(" - %s", ship)));
+        content.append("\n");
+        return this;
+    }
+
+    public ReportBuilder addOperation(String operation)
+    {
+        content.append(String.format("Операция: %s", operation));
+        content.append(System.lineSeparator());
+        return this;
+    }
+
+    public Report build()
+    {
+        return new Report(String.format("Отчет за %s", ZonedDateTime.now().format(DATE_TIME_FORMATTER)),
+                content.toString());
+    }
+
+    StringBuilder content;
+
+}
