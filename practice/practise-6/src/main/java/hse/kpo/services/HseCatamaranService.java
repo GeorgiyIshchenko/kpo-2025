eb09da6fbe52b3680c0288b9b134739a4e64210d
Georgiy Ishchenko <gogaigd2005@gmail.com>
2025-02-23 11:04:52 +0300

    seminar 6 done

--- practice/practise-6/src/main/java/hse/kpo/services/HseCatamaranService.java
+++ practice/practise-6/src/main/java/hse/kpo/services/HseCatamaranService.java
@@ -1,8 +1,15 @@
 package hse.kpo.services;
 
+import hse.kpo.domains.Customer;
+import hse.kpo.enums.ProductionTypes;
 import hse.kpo.interfaces.CustomerProvider;
 import hse.kpo.interfaces.catamarans.CatamaranProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import hse.kpo.observers.SalesObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,6 +21,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class HseCatamaranService {
+
+    final List<SalesObserver> observers = new ArrayList<>();
+
+    public void addObserver(SalesObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObserversForSale(Customer customer, ProductionTypes productType, int vin) {
+        observers.forEach(obs -> obs.onSale(customer, productType, vin));
+    }
 
     private final CatamaranProvider catamaranProvider;
 
@@ -30,6 +47,7 @@ public class HseCatamaranService {
                 .forEach(customer -> {
                     var catamaran = catamaranProvider.takeCatamaran(customer);
                     if (Objects.nonNull(catamaran)) {
+                        notifyObserversForSale(customer, ProductionTypes.CATAMARAN, catamaran.getVin());
                         customer.setCatamaran(catamaran);
                     } else {
                         log.warn("No catamaran in CatamaranService");
